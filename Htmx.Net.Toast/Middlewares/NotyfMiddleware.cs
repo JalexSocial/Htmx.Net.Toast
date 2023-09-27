35faf8e72d2e6397bead47b929c2a09a9001f1fa
Michael Tanczos <tanczosm@eastonsd.org>
2023-09-27 12:13:22 -0400

    Added code to merge existing headers

--- Htmx.Net.Toast/Middlewares/NotyfMiddleware.cs
+++ Htmx.Net.Toast/Middlewares/NotyfMiddleware.cs
@@ -1,4 +1,5 @@
-﻿using Htmx.Net.Toast.Abstractions;
+﻿using System.Collections.Generic;
+using Htmx.Net.Toast.Abstractions;
 using Htmx.Net.Toast.Helpers;
 using Htmx.Net.Toast.Notyf;
 using Htmx.Net.Toast.Notyf.Models;
@@ -8,6 +9,10 @@ using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Primitives;
+using System.Reflection.PortableExecutable;
+using System.Text.Json.Nodes;
+using System.Text.Json;
+using System.Text;
 
 namespace Htmx.Net.Toast.Middlewares;
 
@@ -43,20 +48,88 @@ internal class NotyfMiddleware
 				Configuration = _options.ToJson(),
 				Notifications = _toastNotification.ReadAllNotifications()
 			};
-			if (messages.Notifications != null && messages.Notifications.Any())
+			if (messages.Notifications.Any())
 			{
 				var accessControlExposeHeaders = $"{GetControlExposeHeaders(httpContext.Response.Headers)}";
 				httpContext.Response.Headers.Add(AccessControlExposeHeadersKey, accessControlExposeHeaders);
 
-				var notificationsJson = new { notyfpublish = messages.Notifications }.ToJson();
-				httpContext.Response.Headers.Add(HtmxHeaderKeys.ResponseTriggerAfterSettle,
-					notificationsJson);
+				httpContext.Response.Headers.Add(HtmxHeaderKeys.ResponseTriggerAfterSettle, new { notyfpublish = messages.Notifications }.ToJson());
+
+				var triggers = new Dictionary<string, object?>();
+
+				//var notificationsJson = new { notyfpublish = messages.Notifications }.ToJson();
+
+				if (httpContext.Response.Headers.ContainsKey(HtmxHeaderKeys.ResponseTriggerAfterSettle))
+				{
+					var header = httpContext.Response.Headers[HtmxHeaderKeys.ResponseTriggerAfterSettle];
+
+					// Attempt to parse existing header as Json, if fails it is a simplified event key
+					// assume if the string starts with '{' and ends with '}', that it is JSON
+					if (header.Any(h => h is ['{', .., '}']))
+					{
+						var reader = new Utf8JsonReader(Encoding.UTF8.GetBytes(header));
+						// this might still throw :(
+						var jsonObject = JsonNode.Parse(ref reader)?.AsObject();
+						// Load any existing triggers
+						foreach (var (key, value) in jsonObject!)
+							triggers.Add(key, value);
+					}
+					else
+					{
+						foreach (var headerValue in header)
+						{
+							if (headerValue is null) continue;
+
+							var eventNames = headerValue.Split(',');
+
+							foreach (var eventName in eventNames)
+								triggers.Add(eventName, null);
+						}
+					}
+				}
+
+				triggers.TryAdd("notyfpublish", messages.Notifications);
+
+				var notificationsJson = triggers.ToJson();
+
+				httpContext.Response.Headers[HtmxHeaderKeys.ResponseTriggerAfterSettle] = notificationsJson;
 			}
 		}
 
 		return Task.FromResult(0);
 	}
+	/*
+	private void ParsePossibleExistingTriggers(string headerKey, HtmxTriggerTiming timing)
+	{
+		if (!_headers.ContainsKey(headerKey))
+			return;
 
+		var header = _headers[headerKey];
+		// Attempt to parse existing header as Json, if fails it is a simplified event key
+		// assume if the string starts with '{' and ends with '}', that it is JSON
+		if (header.Any(h => h is ['{', .., '}']))
+		{
+			var reader = new Utf8JsonReader(Encoding.UTF8.GetBytes(header));
+			// this might still throw :(
+			var jsonObject = JsonNode.Parse(ref reader)?.AsObject();
+			// Load any existing triggers
+			foreach (var (key, value) in jsonObject!)
+				WithTrigger(key, value, timing);
+		}
+		else
+		{
+			foreach (var headerValue in _headers[headerKey])
+			{
+				if (headerValue is null) continue;
+
+				var eventNames = headerValue.Split(',');
+
+				foreach (var eventName in eventNames)
+					WithTrigger(eventName, null, timing);
+			}
+		}
+	}
+	*/
 	private object GetControlExposeHeaders(IHeaderDictionary headers)
 	{
 		var existingHeaders = headers[AccessControlExposeHeadersKey];
